6b510889e92bf12d4f5eebdbeb6f79aab566d62f
Djuper <behalserhii@gmail.com>
2019-01-21 11:47:17 +0200

    fixed parser for cookies

--- src/common/save_user_session/save_user_session.py
+++ src/common/save_user_session/save_user_session.py
@@ -1,3 +1,13 @@
+import re
+
+
 def create_cookies_dict_from_string(s):
-    ret = dict((k.strip(), v.strip()) for k, v in (item.split('=') for item in s.split(';')))
-    return ret
+    # ret = dict((k.strip(), v.strip()) for k, v in (item.split('=') for item in s.split(';')))
+    cookie_list = s.split(';')
+    cookie_dict = {}
+    for item in cookie_list:
+        result = re.search('(?P<key>.*)=(?P<value>.*)', item)
+        key = result.group('key')
+        value = result.group('value')
+        cookie_dict.update({key: value})
+    return cookie_dict
